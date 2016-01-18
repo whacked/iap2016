a4bfdaa73c469539186efb91fb75bedb5fbc8c3f
natto <natto@natto-tpy.none>
2016-01-18 07:51:04 -0500

    auto inc

--- src/cljs/iap2016/core.cljs
+++ src/cljs/iap2016/core.cljs
@@ -134,8 +134,8 @@
                {:readOnly "readOnly"
                 :style {:width "100%"
                         :height "4em"
-                        }}
-               (:content msg)]]))
+                        }
+                :value (:content msg)}]]))
     ]
    ;; input area
    [:div
