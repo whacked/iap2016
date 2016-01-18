3ac5b3dee0e55d882407fdda55566136f4610321
natto <natto@natto-tpy.none>
2016-01-18 06:58:26 -0500

    auto inc

--- src/cljs/iap2016/core.cljs
+++ src/cljs/iap2016/core.cljs
@@ -61,9 +61,10 @@
              [:div
               (:username msg) ":"
               [:textarea
-               {:style {:width "100%"
+               {:readOnly "readOnly"
+                :style {:width "100%"
                         :height "4em"
-                        :readOnly "readOnly"}}
+                        }}
                (:content msg)]]))
     ]])
 
