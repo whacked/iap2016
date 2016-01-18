14d304536ecf76faea42aa3dd02c30b2e96e0e81
natto <natto@natto-tpy.none>
2016-01-18 07:16:37 -0500

    auto inc

--- src/cljs/iap2016/core.cljs
+++ src/cljs/iap2016/core.cljs
@@ -4,6 +4,11 @@
             [devtools.core :as devtools]
             
             [goog.dom :as dom]
+            
+            ;; transmission
+            [cljs.core.async :as async :refer (<! >! put! chan)]
+            [taoensso.sente  :as sente :refer (cb-success?)]
+            
             ))
 
 (devtools/enable-feature! :sanity-hints :dirac)
@@ -15,6 +20,17 @@
   "get current time"
   []
   (.getTime (js/Date.)))
+
+;; === transmission ===========================
+(let [{:keys [chsk ch-recv send-fn state]}
+      (sente/make-channel-socket! "/chsk" ; Note the same path as before
+                                  {:type :auto ; e/o #{:auto :ajax :ws}
+                                   })]
+  (def chsk       chsk)
+  (def ch-chsk    ch-recv) ; ChannelSocket's receive channel
+  (def chsk-send! send-fn) ; ChannelSocket's send API fn
+  (def chsk-state state)   ; Watchable, read-only atom
+  )
 
 ;; === hello world ============================
 (defonce app-state
