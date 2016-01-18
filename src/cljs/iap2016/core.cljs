0fca0f6b15fc95e72fff20f9119e1944c0351697
natto <natto@natto-tpy.none>
2016-01-18 06:44:52 -0500

    auto inc

--- src/cljs/iap2016/core.cljs
+++ src/cljs/iap2016/core.cljs
@@ -1,18 +1,40 @@
 (ns iap2016.core
   (:require [reagent.core :as reagent]
+            
             ))
 
 (enable-console-print!)
 
-(defonce app-state (reagent/atom {:text "Hello Chestnut!"}))
+(defonce app-state
+  (reagent/atom
+   {:text "Hello Chestnut!"
+    }))
 
 (defn main-component []
   [:div
    [:h1 (:text @app-state)]
    ])
 
+
+;; chatroom
+(defonce chatroom-state
+  (reagent/atom
+   {:user-list ["bot"]
+    :title "room of chatter"
+    }))
+(defn chatroom-component []
+  [:div
+   "hi all"
+   (apply
+    vector
+    :ul
+    (for [username (@chatroom-state :user-list)]
+      [:li username]))])
+
+;; init app
 (reagent/render-component
- [main-component]
+ ;;main-component
+ chatroom-component
  (js/document.getElementById "app"))
 
 
